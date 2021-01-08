a36fccfd97db75378632d43cc4ac7d4fe9b9a004
karenhakobyanca <karenpufiesarmenia@mail.ru>
2021-01-08 17:21:04 +0400

    another

--- aca.py
+++ aca.py
@@ -1,6 +1,12 @@
-a = 2
-b = 2
-c = a + b
-print(c)
+a = int(input())
+b = int(input())
 
-#new line added
+
+for i in range(a,b+1):
+    oreginal_num = 0
+    count = 0
+    for j in range(1,i+1):
+        if i % j == 0:
+            count+=1
+            oreginal_num = i
+    print(oreginal_num,count)
